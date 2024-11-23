268d8112e054022dfc63b03c66a5bc01c45575dd
Rodrigo Campuzano <camcluod@gmail.com>
2024-11-23 01:10:50 -0600

    fsgnsv

--- app/routers/lista_contacto.py
+++ app/routers/lista_contacto.py
@@ -24,24 +24,34 @@ def create_listacontacto(listacontacto: ListaContactoCreate, db: Session = Depen
 @router.get("/{id_usuario}", response_model=List[ListaContactoResponse])
 def read_listacontacto(id_usuario: int, db: Session = Depends(get_db)):
     listacontacto = db.query(ListaContacto).filter(ListaContacto.id_usuario == id_usuario).all()
+    
     if not listacontacto:
         raise HTTPException(status_code=404, detail="Lista de contactos no encontrada")
+    
     result = []
+    
     for lista in listacontacto:
         usuario_correo = lista.usuario_correo
-        usuario = db.query(Usuario).filter(Usuario.correo_usuario == usuario_correo).all()
+        usuario = db.query(Usuario).filter(Usuario.correo_usuario == usuario_correo).first()  # Usamos first() para obtener un solo usuario
         
-        if usuario:
+        if usuario:  # Si encontramos el usuario
             result.append({
                 "idlista": lista.idlista,
                 "id_usuario": lista.id_usuario,
                 "usuario_correo": lista.usuario_correo,
-                "usuario_id": usuario.id_usuario,  
-                "usuario_nombre": usuario.nombre_usuario  
+                "usuario_id": usuario.id_usuario,  # ID del usuario
+                "usuario_nombre": usuario.nombre_usuario  # Nombre del usuario
+            })
+        else:  # Si no encontramos un usuario para el correo
+            result.append({
+                "idlista": lista.idlista,
+                "id_usuario": lista.id_usuario,
+                "usuario_correo": lista.usuario_correo,
+                "usuario_id": None,  # Si no se encuentra el usuario
+                "usuario_nombre": None  # Si no se encuentra el usuario
             })
 
     return result
-
 
 # Ruta para eliminar una lista de contactos por su ID
 @router.delete("/{lista_id}", response_model=ListaContactoResponse)
