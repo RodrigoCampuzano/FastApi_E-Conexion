2c753e62cea8a23a5abdacae757ef16850121546
Rodrigo Campuzano <camcluod@gmail.com>
2024-11-24 21:32:45 -0600

    dgfvsdc

--- app/routers/foro.py
+++ app/routers/foro.py
@@ -10,16 +10,16 @@ router = APIRouter()
 # Ruta para crear un foro
 @router.post("/", response_model=ForoResponse)
 def create_foro(foro: ForoCreate, db: Session = Depends(get_db)):
-    db_foro = Foro(
+    db_for = Foro(
         id_chat=foro.id_chat,  
         nombre_foro=foro.nombre_foro,
         descripcion=foro.descripcion,
         id_usuario=foro.id_usuario
     )
-    db.add(db_foro)
+    db.add(db_for)
     db.commit()
-    db.refresh(db_foro)  
-    return db_foro
+    db.refresh(db_for)  
+    return db_for
 
 # Ruta para leer un foro por su ID
 @router.get("/{foro_id}", response_model=ForoResponse)
